5bbd9ae11a4056267d4a73915282089dc2c6013f
boghdady <boghdady107@gmail.com>
2020-12-08 14:05:58 +0200

    add eslint, prettier configration

--- app/Main.js
+++ app/Main.js
@@ -5,20 +5,18 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeGuest from './components/HomeGuest';
 
-
 function Main() {
   return (
-<>
-  <Header/>
-  <HomeGuest/>
-  <Footer/>
-</>
-
+    <>
+      <Header />
+      <HomeGuest />
+      <Footer />
+    </>
   );
 }
 
 // Render our app
-ReactDOM.render(<Main/>, document.querySelector('#app'));
+ReactDOM.render(<Main />, document.querySelector('#app'));
 
 // I want the brwoser not do refresh i need only load new javascript automatically to the browser
-if(module.hot) module.hot.accept();
+if (module.hot) module.hot.accept();

cc4b54a77d74a010568dfebac06e0d484d6449bb
boghdady <boghdady107@gmail.com>
2020-12-20 00:51:05 +0200

    view post details

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -27,21 +27,19 @@ function ProfilePosts() {
   return (
     <div className="list-group">
       {posts.map(function (post) {
-        console.log(post);
         const createdDate = new Date(post.createdDate);
-        const convertedData = `${createdDate.getFullYear()}/ ${createdDate.getMonth()}/ ${createdDate.getDay()}`;
+        const convertedData = `${
+          createdDate.getFullYear() + 1
+        }/ ${createdDate.getMonth()}/ ${createdDate.getDay()}`;
         return (
           <Link
             key={post._id}
-            to={`/post/:${post._id}`}
+            to={`/post/${post._id}`}
             className="list-group-item list-group-item-action"
           >
-            <img
-              className="avatar-tiny"
-              src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-            />
+            <img className="avatar-tiny" src={post.author.avatar} />
             {post.author.username}
-            <strong>post.body</strong>
+            <strong>{post.body}</strong>
             <span className="text-muted small">{` on ${convertedData}`} </span>
           </Link>
         );
--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,4 +1,6 @@
-import React, { useContext, useEffect } from 'react';
+import Axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 import StateContext from '../StateContext';
 import Page from './Page';
@@ -6,41 +8,63 @@ import Page from './Page';
 function ViewSinglePost() {
   const appState = useContext(StateContext);
 
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState();
+
+  // Fetch Post data
+  useEffect(() => {
+    async function fetchPost() {
+      const response = await Axios.get(`/post/${id}`);
+      setPost(response.data);
+      setIsLoading(false);
+      console.log(response.data);
+    }
+    fetchPost();
+  }, []);
+
+  if (isLoading)
+    return (
+      <Page title=".....">
+        <div>Loading...</div>
+      </Page>
+    );
+
+  const date = new Date(post.createdDate);
+  const formattedDate = `${
+    date.getFullYear() + 1
+  }/${date.getMonth()}/${date.getDay()}`;
+
   return (
-    <Page title="Post Details">
+    <Page title={post.title}>
       <div>
         <div className="d-flex justify-content-between">
-          <h2>Example Post Title</h2>
+          <h2>{post.title}</h2>
           <span className="pt-2">
-            <a href="#" className="text-primary mr-2" title="Edit">
+            <Link
+              to={`profile/${id}`}
+              className="text-primary mr-2"
+              title="Edit"
+            >
               <i className="fas fa-edit" />
-            </a>
+            </Link>
             <a className="delete-post-button text-danger" title="Delete">
               <i className="fas fa-trash" />
             </a>
           </span>
         </div>
         <p className="text-muted small mb-4">
-          <a href="#">
+          <Link to={`/profile/${post.author.username}`}>
             <img className="avatar-tiny" src={appState.user.avatar} />
-          </a>
-          Posted by <a href="#">brad</a> on 2/10/2020
+          </Link>
+          Posted by{' '}
+          <Link to={`/profile/${post.author.username}`}>
+            {post.author.username}
+          </Link>{' '}
+          {`on ${formattedDate}`}
         </p>
         <div className="body-content">
-          <p>
-            Lorem ipsum dolor sit <strong>example</strong> post adipisicing
-            elit. Iure ea at esse, tempore qui possimus soluta impedit natus
-            voluptate, sapiente saepe modi est pariatur. Aut voluptatibus
-            aspernatur fugiat asperiores at.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod
-            asperiores corrupti omnis qui, placeat neque modi, dignissimos, ab
-            exercitationem eligendi culpa explicabo nulla tempora rem? Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Iure ea at esse,
-            tempore qui possimus soluta impedit natus voluptate, sapiente saepe
-            modi est pariatur. Aut voluptatibus aspernatur fugiat asperiores at.
-          </p>
+          <p>{post.body}</p>
         </div>
       </div>
     </Page>

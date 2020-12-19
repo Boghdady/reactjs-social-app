c63bc68f129e9e63851135cb6c1169ea2e5f3f45
boghdady <boghdady107@gmail.com>
2020-12-19 19:23:58 +0200

    Finish profile posts

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -1,12 +1,12 @@
 import Axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import ContextState from '../StateContext';
 
 function ProfilePosts() {
-  const { username } = useParams();
   const appState = useContext(ContextState);
+  const { username } = useParams();
 
   const [isLoading, setIsloading] = useState(true);
   const [posts, setPosts] = useState([]);
@@ -16,9 +16,9 @@ function ProfilePosts() {
       const response = await Axios.get(`/profile/${username}/posts`, {
         token: appState.user.token,
       });
+
       setPosts(response.data);
       setIsloading(false);
-      console.log(posts);
     }
     fetchProfilePosts();
   }, []);
@@ -26,15 +26,26 @@ function ProfilePosts() {
   if (isLoading) return <div>Loading...</div>;
   return (
     <div className="list-group">
-      <a href="#" className="list-group-item list-group-item-action">
-        <img
-          className="avatar-tiny"
-          src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-        />
-        {'{'}' '{'}'}
-        <strong>Example Post #1</strong>
-        <span className="text-muted small">on 2/10/2020 </span>
-      </a>
+      {posts.map(function (post) {
+        console.log(post);
+        const createdDate = new Date(post.createdDate);
+        const convertedData = `${createdDate.getFullYear()}/ ${createdDate.getMonth()}/ ${createdDate.getDay()}`;
+        return (
+          <Link
+            key={post._id}
+            to={`/post/:${post._id}`}
+            className="list-group-item list-group-item-action"
+          >
+            <img
+              className="avatar-tiny"
+              src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
+            />
+            {post.author.username}
+            <strong>post.body</strong>
+            <span className="text-muted small">{` on ${convertedData}`} </span>
+          </Link>
+        );
+      })}
     </div>
   );
 }

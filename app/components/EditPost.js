5b44dc716e5ff6288c45ff70f55d41543b1a2a45
boghdady <boghdady107@gmail.com>
2020-12-23 23:45:52 +0200

    Working on edit post

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -1,14 +1,48 @@
 import Axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useImmerReducer } from 'use-immer';
 
+import DispatchContext from '../DispatchContext';
 import StateContext from '../StateContext';
 import Page from './Page';
 
 function EditPost() {
   const appState = useContext(StateContext);
-  const { id } = useParams();
+  const appDispatch = useContext(DispatchContext);
+
   const [post, setPost] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const InitState = {
+    title: {
+      value: '',
+      hasError: false,
+      errorMsg: '',
+    },
+    body: {
+      value: '',
+      hasError: false,
+      errorMsg: '',
+    },
+    isFetching: true,
+    isPostSaved: false,
+    id: useParams().id,
+    sendCount: 0,
+  };
+  function ourReducer(draftOfState, action) {
+    switch (action.type) {
+      case 'fetchPostComplete':
+        return;
+    }
+  }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, InitState);
+
+  function submitHandler(e) {
+    e.preventDefault();
+    dispatch('submitRequest');
+  }
 
   // Fetch Post data
   useEffect(() => {
@@ -19,23 +53,26 @@ function EditPost() {
       });
       setPost(response.data);
       console.log(response.data);
-      // setIsLoading(false);
+      setPost(response.data);
+      setIsLoading(false);
     }
     fetchPost();
     return () => {
       ourRequest.cancel();
     };
   }, []);
+
+  if (isLoading) return <div>Loading.....</div>;
   return (
     <Page title="Eidt Post">
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="form-group">
           <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
           </label>
           <input
             autoFocus
-            // value={post.title}
+            value={post.title}
             name="title"
             id="post-title"
             className="form-control form-control-lg form-control-title"
@@ -49,6 +86,7 @@ function EditPost() {
             <small>Body Content</small>
           </label>
           <textarea
+            value={post.body}
             name="body"
             id="post-body"
             className="body-content tall-textarea form-control"

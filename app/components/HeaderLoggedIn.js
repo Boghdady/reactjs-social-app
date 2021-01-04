a6672111a7a3dd1a4aa6979c20b6573d0516421e
boghdady <boghdady107@gmail.com>
2021-01-05 00:29:11 +0200

    Add header tooltip

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import ReactTooltip from 'react-tooltip';
 
 import DispatchContext from '../DispatchContext';
 import StateContext from '../StateContext';
@@ -23,18 +24,32 @@ function HeaderLoggedIn() {
     <div className="flex-row my-3 my-md-0">
       <a
         onClick={handleSearchIcon}
+        data-for="search"
+        data-tip="Search"
         href="#"
         className="text-white mr-2 header-search-icon"
       >
         <i className="fas fa-search" />
       </a>
-      <span className="mr-2 header-chat-icon text-white">
+      <ReactTooltip place="bottom" id="search" className="custom-tooltip" />
+      <span
+        className="mr-2 header-chat-icon text-white"
+        data-for="chat"
+        data-tip="Chat"
+      >
         <i className="fas fa-comment" />
         <span className="chat-count-badge text-white"> </span>
       </span>
-      <Link to={`/profile/${appState.user.username}`} className="mr-2">
+      <ReactTooltip place="bottom" id="chat" className="custom-tooltip" />
+      <Link
+        data-for="profile"
+        data-tip="My Profile"
+        to={`/profile/${appState.user.username}`}
+        className="mr-2"
+      >
         <img className="small-header-avatar" src={appState.user.avatar} />
       </Link>
+      <ReactTooltip place="bottom" id="profile" className="custom-tooltip" />
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
       </Link>
